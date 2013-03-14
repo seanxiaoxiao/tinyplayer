ce9ae4518b5046b124c48f36635913e2ed88d08a
clydeli <clyde1008li@gmail.com>
2013-03-14 09:58:18 -0700

    merge conflicts

--- index.js
+++ index.js
@@ -110,9 +110,10 @@ var tinyPlayer = tinyPlayer || {};
       songRow.click(function(index) {
         return function() {
           drivePlayer.playerInstance.play(index);
-          drivePlayer.refreshDancer();
-          drivePlayer.dancerInstance.play();
-          tinyPlayer.updateSongTitle();
+          drivePlayer.refreshUI();
+          //drivePlayer.refreshDancer();
+          //drivePlayer.dancerInstance.play();
+          //tinyPlayer.updateSongTitle();
           $('#toggleButton').removeClass('paused');
         }
       }(i));
