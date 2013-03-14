ee13c3ab2f56ad976dc6a9c28dd08775152eeffb
Sumeet Kumar <kr.sumeet@gmail.com>
2013-03-14 03:23:47 -0700

    moved playlist code to another file

--- player.js
+++ player.js
@@ -1,4 +1,5 @@
 var drivePlayer = drivePlayer || {};
+
 var allSongs;
 
 (function () {
@@ -24,7 +25,7 @@ var allSongs;
         },
 
         createPlaylist:function () {
-            var uploadFunc = this.uploadPlaylist;
+            var uploadFunc = playlist.uploadPlaylist;
             $.get("https://www.googleapis.com/drive/v2/files?access_token=" + this.googleAuthInstance.getAccessToken(), function (data) {
                 console.log(data);
                 var playlistContainer = $("#current-list tbody");
@@ -64,59 +65,6 @@ var allSongs;
 
                 uploadFunc("newplaylist", JSON.stringify(allSongs));
             });
-        },
-
-        getPlaylists:function () {
-            q = "title contains playlist";
-            $.get("https://www.googleapis.com/drive/v2/files?access_token=" + this.googleAuthInstance.getAccessToken() + "&q=" + q, function (data) {
-                console.log(data);
-
-            });
-        },
-
-        uploadPlaylist:function (playlistName, data) {
-            var boundary = '-------314159265358979323846';
-            var body = '--' + boundary + '\r\n'
-                // Parameter name is "file" and local filename is "temp.txt"
-                + 'Content-Disposition: form-data; name="file";'
-                + 'title=playlistName\r\n'
-                // Add the file's mime-type
-                + 'Content-type: plain/text\r\n\r\n'
-                // Add your data:
-                + data + '\r\n'
-                + boundary + '--';
-
-            $.ajax({
-                contentType:"multipart/related",
-                data:body,
-                type:"POST",
-                dataType:"json",
-                url:"https://www.googleapis.com/upload/drive/v2/files?access_token=" + drivePlayer.googleAuthInstance.getAccessToken() +
-                    "&uploadType=multipart",
-
-                success:function (data, status) {
-                    console.log("success");
-                },
-
-                error:function () {
-                    console.log('Failed!');
-                }
-            });
-
-            /*var data = {
-             'type': 'user',
-             'role': 'reader',
-             'value': 'sean.xiao@west.cmu.edu'
-             };
-             $.ajax({
-             url:"https://www.googleapis.com/drive/v2/files/1SiGLL_GoOZypHQd-M3bf4Y-AVCZCMFwzUwLgEjzNbgQ/permissions?access_token="+this.googleAuthInstance.getAccessToken(),
-             type:"POST",
-             data: JSON.stringify(data),
-             contentType:"application/json; charset=utf-8",
-             dataType:"json",
-             error: function(data){console.log(data); }
-             });*/
-
         },
 
         /**
--- playlist.js
+++ playlist.js
@@ -1,81 +1,59 @@
-//function addPlaylist(fileData, callback) {
-//
-//    const boundary = '-------314159265358979323846';
-//    const delimiter = "\r\n--" + boundary + "\r\n";
-//    const close_delim = "\r\n--" + boundary + "--";
-//
-//    var reader = new FileReader();
-//    reader.readAsBinaryString(fileData);
-//    reader.onload = function (e) {
-//        var contentType = fileData.type || 'application/octet-stream';
-//        var metadata = {
-//            'title':"playlist",
-//            'mimeType':contentType
-//        };
-//
-//        var base64Data = btoa(reader.result);
-//        var multipartRequestBody =
-//            delimiter +
-//                'Content-Type: application/json\r\n\r\n' +
-//                JSON.stringify(metadata) +
-//                delimiter +
-//                'Content-Type: ' + contentType + '\r\n' +
-//                'Content-Transfer-Encoding: base64\r\n' +
-//                '\r\n' +
-//                base64Data +
-//                close_delim;
-//
-//
-//        $.post(
-//            "https://www.googleapis.com/upload/drive/v2/files?access_token=" + this.googleAuthInstance.getAccessToken() +
-//                "&uploadType=multipart", function (multipartRequestBody) {
-//
-//            })
-//            .done(function () {
-//                console.log("second success");
-//            })
-//            .fail(function () {
-//                console.log("error");
-//            })
-//            .always(function () {
-//                console.log("finished");
-//            });
-//        ;
-//    }
-//}
-//;
-//function addSong(playlistName, song) {
-//    var song;
-//}
-//;
+var playlist = playlist || {};
 
-//function uploadPlaylist(playlistName, data) {
-//    var boundary = '-------314159265358979323846';
-//    var body = '--' + boundary + '\r\n'
-//        // Parameter name is "file" and local filename is "temp.txt"
-//        + 'Content-Disposition: form-data; name="file";'
-//        + 'title=playlistName\r\n'
-//        // Add the file's mime-type
-//        + 'Content-type: plain/text\r\n\r\n'
-//        // Add your data:
-//        +  data + '\r\n'
-//        + boundary + '--';
-//
-//    $.ajax({
-//        contentType:"multipart/related",
-//        data:body,
-//        type:"POST",
-//        dataType:"json",
-//        url:"https://www.googleapis.com/upload/drive/v2/files?access_token=" + drivePlayer.googleAuthInstance.getAccessToken() +
-//            "&uploadType=multipart",
-//
-//        success:function (data, status) {
-//            console.log("success");
-//        },
-//
-//        error: function() { console.log('Failed!'); }
-//    });
-//}
+(function () {
 
+    playlist = {
+    getPlaylists:function() {
+        q = "title contains playlist";
+        $.get("https://www.googleapis.com/drive/v2/files?access_token=" + this.googleAuthInstance.getAccessToken() + "&q=" + q, function (data) {
+            console.log(data);
 
+        });
+    }
+    ,
 
+    uploadPlaylist:function (playlistName, data) {
+        var boundary = '-------314159265358979323846';
+        var body = '--' + boundary + '\r\n'
+            // Parameter name is "file" and local filename is "temp.txt"
+            + 'Content-Disposition: form-data; name="file";'
+            + 'title=playlistName\r\n'
+            // Add the file's mime-type
+            + 'Content-type: plain/text\r\n\r\n'
+            // Add your data:
+            + data + '\r\n'
+            + boundary + '--';
+
+        $.ajax({
+            contentType:"multipart/related",
+            data:body,
+            type:"POST",
+            dataType:"json",
+            url:"https://www.googleapis.com/upload/drive/v2/files?access_token=" + drivePlayer.googleAuthInstance.getAccessToken() +
+                "&uploadType=multipart",
+
+            success:function (data, status) {
+                console.log("success");
+            },
+
+            error:function () {
+                console.log('Failed!');
+            }
+        })
+    }
+    }
+})();
+
+/*var data = {
+ 'type': 'user',
+ 'role': 'reader',
+ 'value': 'sean.xiao@west.cmu.edu'
+ };
+ $.ajax({
+ url:"https://www.googleapis.com/drive/v2/files/1SiGLL_GoOZypHQd-M3bf4Y-AVCZCMFwzUwLgEjzNbgQ/permissions?access_token="+this.googleAuthInstance.getAccessToken(),
+ type:"POST",
+ data: JSON.stringify(data),
+ contentType:"application/json; charset=utf-8",
+ dataType:"json",
+ error: function(data){console.log(data); }
+ });*/
